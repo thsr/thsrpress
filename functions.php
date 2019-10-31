c9f3e18e7d5742dce800346bf4c4a94bc2f419fe
thsr <437808@gmail.com>
2019-10-31 11:41:54 +0100

    init thsrpress

---
+++ functions.php
@@ -0,0 +1,177 @@
+<?php
+
+/* THEME SETUP
+------------------------------------------------ */
+
+if ( ! function_exists( 'davis_setup' ) ) :
+
+    function davis_setup() {
+
+        // Automatic feed
+        add_theme_support( 'automatic-feed-links' );
+
+        // Set content-width
+        global $content_width;
+        if ( ! isset( $content_width ) ) $content_width = 620;
+
+        // Post thumbnails
+        add_theme_support( 'post-thumbnails' );
+        add_image_size( 'post-image', 620, 9999 );
+
+        // Title tag
+        add_theme_support( 'title-tag' );
+
+        // Post formats
+        add_theme_support( 'post-formats', array( 'aside' ) );
+
+        // Add nav menu
+        register_nav_menu( 'primary-menu', __( 'Primary Menu', 'davis' ) );
+
+        // Make the theme translation ready
+        load_theme_textdomain( 'davis', get_template_directory() . '/languages' );
+
+        $locale_file = get_template_directory() . "/languages/" . get_locale();
+
+        if ( is_readable( $locale_file ) ) {
+            require_once( $locale_file );
+        }
+
+    }
+    add_action( 'after_setup_theme', 'davis_setup' );
+endif;
+
+
+/* ENQUEUE STYLES
+------------------------------------------------ */
+
+if ( ! function_exists( 'davis_load_style' ) ) :
+    function davis_load_style() {
+
+        $theme_version = wp_get_theme( 'davis' )->get( 'Version' );
+
+        if ( ! is_admin() ) {
+            wp_register_style( 'davis_fonts', '//fonts.googleapis.com/css?family=PT+Serif:400,700,400italic,700italic' );
+            wp_enqueue_style( 'davis_style', get_stylesheet_uri(), array( 'davis_fonts' ) );
+        }
+
+    }
+    add_action( 'wp_enqueue_scripts', 'davis_load_style' );
+endif;
+
+
+/* ENQUEUE COMMENT-REPLY.JS
+------------------------------------------------ */
+
+if ( ! function_exists( 'davis_load_scripts' ) ) :
+    function davis_load_scripts() {
+
+        $theme_version = wp_get_theme( 'davis' )->get( 'Version' );
+
+        wp_enqueue_script( 'davis_construct', get_template_directory_uri() . '/assets/js/construct.js', array( 'jquery' ), $theme_version, true );
+
+        if ( ( ! is_admin() ) && is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
+            wp_enqueue_script( 'comment-reply' );
+        }
+
+    }
+    add_action( 'wp_enqueue_scripts', 'davis_load_scripts' );
+endif;
+
+
+/* BODY CLASSES
+------------------------------------------------ */
+
+if ( ! function_exists( 'davis_body_classes' ) ) :
+    function davis_body_classes( $classes ) {
+
+        // Check whether we want it darker
+        if ( get_theme_mod( 'davis_dark_mode' ) ) {
+            $classes[] = 'dark-mode';
+        }
+
+        return $classes;
+
+    }
+    add_action( 'body_class', 'davis_body_classes' );
+endif;
+
+
+/* CUSTOMIZER SETTINGS
+------------------------------------------------ */
+
+class davis_customize {
+
+    public static function davis_register ( $wp_customize ) {
+
+        // Dark Mode
+        $wp_customize->add_setting( 'davis_dark_mode', array(
+            'capability'         => 'edit_theme_options',
+            'sanitize_callback' => 'davis_sanitize_checkbox',
+            'transport'            => 'postMessage'
+        ) );
+
+        $wp_customize->add_control( 'davis_dark_mode', array(
+            'type'             => 'checkbox',
+            'section'         => 'colors', // Default WP section added by background_color
+            'label'         => __( 'Dark Mode', 'davis' ),
+            'description'     => __( 'Displays the site with white text and black background. If Background Color is set, only the text color will change.', 'davis' ),
+        ) );
+
+
+        // Make built-in controls use live JS preview
+        $wp_customize->get_setting( 'blogname' )->transport = 'postMessage';
+        $wp_customize->get_setting( 'background_color' )->transport = 'postMessage';
+
+
+        // SANITATION
+
+        // Sanitize boolean for checkbox
+        function davis_sanitize_checkbox( $checked ) {
+            return ( ( isset( $checked ) && true == $checked ) ? true : false );
+        }
+
+    }
+
+    // Initiate the live preview JS
+    public static function davis_live_preview() {
+        wp_enqueue_script( 'davis-themecustomizer', get_template_directory_uri() . '/assets/js/theme-customizer.js', array(  'jquery', 'customize-preview' ), '', true );
+    }
+
+}
+
+// Setup the Theme Customizer settings and controls
+add_action( 'customize_register', array( 'davis_customize', 'davis_register' ) );
+
+// Enqueue live preview javascript in Theme Customizer admin screen
+add_action( 'customize_preview_init', array( 'davis_customize' , 'davis_live_preview' ) );
+
+
+/* ---------------------------------------------------------------------------------------------
+   SPECIFY GUTENBERG SUPPORT
+------------------------------------------------------------------------------------------------ */
+
+
+if ( ! function_exists( 'davis_add_gutenberg_features' ) ) :
+    function davis_add_gutenberg_features() {
+
+        /* Gutenberg Palette --------------------------------------- */
+
+        add_theme_support( 'editor-color-palette', array(
+            array(
+                'name'     => _x( 'Black', 'Name of the black color in the Gutenberg palette', 'davis' ),
+                'slug'     => 'black',
+                'color' => '#000',
+            ),
+            array(
+                'name'     => _x( 'White', 'Name of the white color in the Gutenberg palette', 'davis' ),
+                'slug'     => 'white',
+                'color' => '#fff',
+            ),
+        ) );
+
+    }
+    add_action( 'after_setup_theme', 'davis_add_gutenberg_features' );
+endif;
+
+
+?>
---
+++ index.php
@@ -0,0 +1,197 @@
+<!DOCTYPE html>
+
+<html class="no-js" <?php language_attributes(); ?>>
+
+    <head>
+
+        <meta http-equiv="content-type" content="<?php bloginfo( 'html_type' ); ?>" charset="<?php bloginfo( 'charset' ); ?>" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" >
+
+        <link rel="profile" href="http://gmpg.org/xfn/11">
+
+        <link href="https://fonts.googleapis.com/css?family=IBM+Plex+Mono|IBM+Plex+Sans:300,400,500,700&display=swap" rel="stylesheet">
+
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/9.15.10/styles/atelier-forest-light.min.css">
+        <script src="//cdnjs.cloudflare.com/ajax/libs/highlight.js/9.15.10/highlight.min.js"></script>
+        <script>hljs.initHighlightingOnLoad();</script>
+
+        <?php wp_head(); ?>
+
+    </head>
+
+    <body <?php body_class(); ?>>
+
+        <?php if ( function_exists( 'wp_body_open' ) ) { wp_body_open(); } ?>
+
+        <a class="skip-link screen-reader-text" href="#site-content"><?php _e( 'Skip to the content', 'davis' ); ?></a>
+        <a class="skip-link screen-reader-text" href="#menu-menu"><?php _e( 'Skip to the main menu', 'davis' ); ?></a>
+
+        <header class="site-header" role="banner">
+
+            <button type="button" class="toggle-menu" onclick="document.querySelector('body').classList.toggle('show-menu')">&#9776;</button>
+
+            <?php if ( has_nav_menu( 'primary-menu' ) ) : ?>
+
+                <nav role="navigation">
+                    <?php wp_nav_menu( array( 'theme_location' => 'primary-menu' ) ); ?>
+                </nav>
+
+            <?php endif; ?>
+
+            <?php $site_title_elem = is_front_page() ? 'h1' : 'div'; ?>
+
+            <<?php echo $site_title_elem; ?> class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>"><?php bloginfo( 'name' ); ?></a></<?php echo $site_title_elem; ?>>
+
+            <?php if ( get_bloginfo( 'description' ) ) : ?>
+                <p class="site-description"><?php bloginfo( 'description' ); ?></p>
+            <?php endif; ?>
+
+        </header><!-- header -->
+
+        <main class="wrapper" id="site-content" role="main">
+
+            <?php if ( is_archive() || is_search() ) :
+
+                if ( is_search() ) {
+                    global $wp_query;
+                    // Translators: %s = The search query
+                    $archive_title = sprintf( _x( 'Search Results: &ldquo;%s&rdquo;', '%s = The search query', 'davis' ), get_search_query() );
+                    $archive_description = sprintf( _nx( '%s result was found.', '%s results were found.', $wp_query->found_posts, '%s = The search query', 'davis' ), $wp_query->found_posts );
+                } else {
+                    $archive_title = get_the_archive_title();
+                    $archive_description = get_the_archive_description();
+                }
+                ?>
+
+                <header class="archive-header">
+                    <?php if ( $archive_title ) : ?>
+                        <h1 class="archive-title"><?php echo $archive_title; ?></h1>
+                    <?php endif; ?>
+                    <?php if ( $archive_description ) : ?>
+                        <div class="archive-description"><?php echo $archive_description; ?></div>
+                    <?php endif; ?>
+                </header>
+
+            <?php endif; ?>
+
+            <?php if ( have_posts() ) :
+
+                while ( have_posts() ) : the_post(); ?>
+
+                    <div <?php post_class( 'post' ); ?>>
+
+                        <?php if ( ! get_post_format() == 'aside' ) : ?>
+
+                            <?php $post_title_elem = is_single() ? 'h1' : 'h2'; ?>
+                            <<?php echo $post_title_elem ?> class="entry-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></<?php echo $post_title_elem ?>>
+
+                        <?php endif; ?>
+                        <?php
+
+                        $post_type = get_post_type();
+
+                        if ( $post_type == 'post' ) : ?>
+                        <div class="entry-subtitle">
+                            <a href="<?php the_permalink(); ?>"><time datetime="<?php the_modified_time('Y-m-d'); ?>"><?php the_modified_time(get_option('date_format')); ?></time></a>
+                            <span class="sep"></span>
+                            <?php the_category( ', ' ); ?>
+
+                        </div>
+                        <?php endif; ?>
+
+                        <?php if ( has_post_thumbnail() && ! post_password_required() ) : ?>
+
+                            <a href="<?php the_permalink(); ?>" class="featured-image">
+                                <?php the_post_thumbnail( 'post-image' ); ?>
+                            </a>
+
+                        <?php endif; ?>
+
+                        <div class="content">
+
+                            <?php the_content(); ?>
+
+                        </div><!-- .content -->
+
+                        <?php
+
+                        if ( is_singular() ) wp_link_pages();
+
+                        $post_type = get_post_type();
+
+                        if ( $post_type == 'post' ) : ?>
+
+                            <div class="meta">
+
+<!--                                 <p> -->
+
+<!--                                     <a href="<?php the_permalink(); ?>">Updated <time datetime="<?php the_modified_time('Y-m-d'); ?>"><?php the_modified_time(get_option('date_format')); ?></time></a> -->
+
+<!--                                     <?php if ( comments_open() && ! post_password_required() ) : ?> -->
+<!--                                         <span class="sep"></span><?php comments_popup_link( __( 'Add Comment', 'davis' ), __( '1 Comment', 'davis' ), '% ' . __( 'Comments', 'davis' ), '', __( 'Comments off', 'davis' ) ); ?> -->
+<!--                                     <?php endif; ?> -->
+
+<!--                                     <?php if ( is_sticky() ) : ?> -->
+<!--                                         <span class="sep"></span><?php _e( 'Sticky', 'davis' ); ?> -->
+<!--                                     <?php endif ?> -->
+
+<!--                                 </p> -->
+
+<!--                                 <?php edit_post_link($text="edit", $before='<p>', $after='</p>'); ?> -->
+
+                                <?php if ( is_singular( 'post' ) ) : ?>
+
+<!--                                     <p><?php the_category( ', ' ); ?></p> -->
+                                    <p><?php the_tags( ' #', ' #', ' ' ); ?></p>
+
+                                <?php endif; ?>
+
+                            </div><!-- .meta -->
+
+                        <?php endif;
+
+                        if ( ( $post_type == 'post' || comments_open() || get_comments_number() ) && ! post_password_required() ) {
+                            comments_template();
+                        }
+
+                        ?>
+
+                    </div><!-- .post -->
+
+                    <?php
+
+                endwhile;
+
+            else : ?>
+
+                <div class="post">
+
+                    <p><?php _e( 'Sorry, the page you requested cannot be found.', 'davis' ); ?></p>
+
+                </div><!-- .post -->
+
+            <?php endif;
+
+            if ( ! is_singular() && ( get_previous_posts_link() || get_next_posts_link() ) ) : ?>
+
+                <div class="pagination">
+
+                    <?php previous_posts_link( '&larr; ' . __( 'Newer posts', 'davis' ) ); ?>
+                    <?php next_posts_link( __( 'Older posts', 'davis') . ' &rarr;' ); ?>
+
+                </div><!-- .pagination -->
+
+            <?php endif; ?>
+
+            <footer role="contentinfo">
+
+                <p><a href="<?php echo esc_url( home_url() ); ?>">&copy; <?php bloginfo( 'name' ); ?> <?php echo date( 'Y' ); ?></a></p>
+
+            </footer><!-- footer -->
+
+        </main><!-- .wrapper -->
+
+        <?php wp_footer(); ?>
+
+    </body>
+</html>
